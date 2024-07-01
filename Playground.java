e3f9cb91f67883731fc4f2a3bc9c2b76e2067cb9
Tiago Cancela <tiagoalmeidacancela@gmail.com>
2024-07-01 11:53:39 +0100

    Update Playground.java

--- Playground.java
+++ Playground.java
@@ -1,11 +1,16 @@
 public class Playground {
 
     public static void main(String[]args){
-            RoomType R1 = new RoomType
+            RoomType Single = new RoomType(Single, 100);
+            RoomType Double = new RoomType(Double, 150);
+            RoomType Suite = new RoomType(Suite, 250);
 
+            System.out.println("name " + RoomType.getname() + " price=" + RoomType.getPrice());
 
-
-
+            Room 101 = new Room(101, Single);
+            Room 102 = new Room(102, Single);
+            Room 103 = new Room(103, Suite);
+            Room 104 = new Room(104, Suite);
 
     }
 }
